8fa9757b492689e7fec23ec4a3b3e85270b5fb95
FaMaVa <faithvaz@Faiths-MacBook-Air.local>
2023-03-16 11:21:14 -0600

    final final

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -17,7 +17,7 @@ export default function About() {
               <Image src={require('../../assets/VazEJHS0191art.jpg')} roundedCircle id="image" />
             </Row>
             <Row className='about'>
-            A Full-Stack web developer, who is eager to apply my knowledge and skills, while gaining more valuable experience and understanding, within the industry. I am well-versed in JavaScript and have the ability to work both front-end and back-end. I possess strong communication skills and have experience in working alongside a team to create high-quality applications. I also have a high level of determination and attention to detail when it comes to problem-solving and building a  website.            
+            A Full-Stack web developer, who is eager to apply my knowledge and skills, while gaining more valuable experience and understanding within the industry. I am well-versed in JavaScript and have the ability to work both front-end and back-end. I possess strong communication skills and have experience in working alongside a team to create high-quality applications. I also have a high level of determination and attention to detail when it comes to problem-solving and building a  website.            
             </Row>
           </Col>
         </Row>
